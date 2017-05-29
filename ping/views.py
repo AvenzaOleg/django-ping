764fc2d08cbcdaf15fbfa01e725fb5883061f306
Oleg Serov <oleg@serov.name>
2017-05-29 13:54:44 -0400

    Minor improvements

--- ping/views.py
+++ ping/views.py
@@ -2,7 +2,6 @@ import json
 
 from django.http import HttpResponse
 from django.conf import settings
-from django.contrib.auth.decorators import login_required
 from django.views.decorators.csrf import csrf_exempt
 
 from ping.defaults import *
