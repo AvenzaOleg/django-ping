ef7253219beb4ef9dbd3a8c621e88317789e8b97
Oleg Serov <oleg@serov.name>
2017-06-01 12:50:23 -0400

    Added authorization

--- ping/decorators.py
+++ ping/decorators.py
@@ -2,10 +2,14 @@ from functools import wraps
 import base64
 
 from django.http import HttpResponse
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import (
+    authenticate,
+    login
+)
 from django.conf import settings
 
 from ping.defaults import PING_BASIC_AUTH
+
 
 def http_basic_auth(func):
     """
@@ -18,18 +22,17 @@ def http_basic_auth(func):
     from
     http://djangosnippets.org/snippets/1304/
     """
+
     @wraps(func)
     def _decorator(request, *args, **kwargs):
         if getattr(settings, 'PING_BASIC_AUTH', PING_BASIC_AUTH):
-            from django.contrib.auth import authenticate, login
             if request.META.has_key('HTTP_AUTHORIZATION'):
                 authmeth, auth = request.META['HTTP_AUTHORIZATION'].split(' ', 1)
                 if authmeth.lower() == 'basic':
                     auth = auth.strip().decode('base64')
                     username, password = auth.split(':', 1)
-                    user = authenticate(username=username, password=password)
-                    if user:
-                        login(request, user)
+
+                    if (username, password) == settings.PING_BASIC_AUTH:
                         return func(request, *args, **kwargs)
                     else:
                         return HttpResponse("Invalid Credentials", status=401)
@@ -37,4 +40,5 @@ def http_basic_auth(func):
                 return HttpResponse("No Credentials Provided", status=401)
         else:
             return func(request, *args, **kwargs)
+
     return _decorator
